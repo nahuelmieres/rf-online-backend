8a61dd4334cc4303da10479aa7d5db3d15d7f506
Nahuel <nmiereschiva@gmail.com>
2025-07-03 10:27:05 -0300

    Añado función para obtener usuarios con paginación y filtros en el controlador de usuarios.

--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { registrarUsuario, loginUsuario, asignarPlanificacion, obtenerPerfil } = require('../controllers/usuariosController');
+const { registrarUsuario, loginUsuario, asignarPlanificacion, 
+    obtenerPerfil, obtenerUsuarios } = require('../controllers/usuariosController');
 const verificarToken = require('../middlewares/authMiddleware');
 const verificarRol = require('../middlewares/rolMiddleware');
 
 router.post('/registrar', registrarUsuario);
+
 router.post('/login', loginUsuario);
 
 router.get('/perfil', verificarToken, obtenerPerfil);
 
 router.put('/asignar-plan/:idPlan', verificarToken, verificarRol('admin', 'coach'), asignarPlanificacion);
+
+router.get('/clientes', verificarToken, verificarRol('admin', 'coach'), obtenerUsuarios);
 
 module.exports = router;
