5b712b7959e2881e96cf4dcd9cb70fa97c4f5f88
Nahuel <nmiereschiva@gmail.com>
2025-08-08 16:19:10 +0200

    feat: agregué endpoint para obtener todas las reservas con paginación y filtros por fecha, sucursal y tipo.

--- src/routes/reservasRoutes.js
+++ src/routes/reservasRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { obtenerDisponibilidad, crearReserva,
-  obtenerMisReservas, eliminarReserva
+    obtenerMisReservas, eliminarReserva,
+    obtenerTodasReservas
 } = require('../controllers/reservasController');
 const verificarToken = require('../middlewares/authMiddleware');
+const verificarRol = require('../middlewares/rolMiddleware');
 
 router.get('/disponibilidad/', verificarToken, obtenerDisponibilidad);
 router.post('/', verificarToken, crearReserva);
 router.get('/mis-reservas', verificarToken, obtenerMisReservas);
 router.delete('/:id', verificarToken, eliminarReserva);
+router.get('/', verificarToken, verificarRol('coach', 'admin'), obtenerTodasReservas);
 
 module.exports = router;
